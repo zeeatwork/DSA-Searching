d289b7fccdba93c3bd7885f97020493c2831a43d
Zenzi Ali <zeeathome935@gmail.com>
2020-09-30 13:59:17 -0400

    added sample data library

---
+++ library.js
@@ -0,0 +1,40 @@
+const library = [
+  { author: "Cowlishaw, Mike", dewey: "005.133", title: "The REXX Language" },
+  { author: "Sams", dewey: "005.133", title: "Teach Yourself C++ In 21 Days" },
+  {
+    author: "Stroustrup., Bjarne",
+    dewey: "005.133",
+    title: "The C++ Programming Language",
+  },
+  {
+    author: "Crockford, Douglas",
+    dewey: "005.2762",
+    title: "JavaScript: The Good Parts",
+  },
+  {
+    author: "Flanagan, David",
+    dewey: "005.2762",
+    title: "JavaScript: The Definitive Guide",
+  },
+  {
+    author: "Schmidt, Meinhard",
+    dewey: "005.44684",
+    title: "Windows Vista for Dummies",
+  },
+  { author: "Zondervan", dewey: "220.52081", title: "NIV Study Bible" },
+  {
+    author: "Humphries, Russell, Dr.",
+    dewey: "231.7652",
+    title: "Starlight and Time",
+  },
+  {
+    author: "Jane, Frederick Thomas",
+    dewey: "623.82509051",
+    title: "Jane's Fighting Ships",
+  },
+  {
+    author: "Norris, Chuck",
+    dewey: "796.8092",
+    title: "The Official Chuck Norris Fact Book",
+  },
+];
